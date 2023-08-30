8aa84029d253534f45b185d31b3dfa52a08d5b0f
SarthakHatwar <137857501+SarthakHatwar@users.noreply.github.com>
2023-08-31 03:26:11 +0530

    committ

---
+++ config/mongoose.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const DB = 'mongodb://0.0.0.0:27017/ERSS'
+
+
+mongoose.connect(DB).then(()=>{
+     console.log('connection successful');
+ }).catch((err) => console.log("no connection " + err));
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open',  function(){
+     console.log('Connected to Database :: MongoDB');
+});
+
+ 
+module.exports = db;  
