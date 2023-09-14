b142b8ec1c38be850886b16958f9e1ee5cfa6aed
SarthakHatwar <137857501+SarthakHatwar@users.noreply.github.com>
2023-09-14 21:42:15 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const DB = 'mongodb+srv://User21:test123@cluster0.djvmwix.mongodb.net/?retryWrites=true&w=majority'
 
 
-mongoose.connect( process.env.MONGO_URI).then(()=>{
+
+mongoose.connect('mongodb+srv://shatwar55:dream2020@cluster0.p2zrhab.mongodb.net/?retryWrites=true&w=majority').then(()=>{
      console.log('connection successful');
  }).catch((err) => console.log("no connection " + err));
 
